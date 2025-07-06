e4c605ddf6998aee0d51ff6b7831b5a83313009d
KennethHernan <kdelacruz18@autonoma.edu.pe>
2025-07-06 17:50:22 -0500

    Update Logo

--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -22,7 +22,7 @@ function HomePage() {
                 {/* Seccion 1 */}
                 <div className="h-[100vh]" id="inicio">
                     <section className="h-[90px] flex justify-between text-[#fff] px-10 py-3">
-                        <img src={Logo} className="w-[120px]" />
+                        <p className="w-[120px] font-bold text-[30px]">Triba</p>
                         <div className="h-auto flex gap-5 items-center justify-center">
                             <a href="#inicio">
                                 <button className="hover:bg-[#00000084] px-4 py-2 rounded-[10px] transition-colors duration-800">Inicio</button>
